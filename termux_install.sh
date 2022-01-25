7ebab1e7ca5914f76bc998b41d6e9bcc2fbdc591
PunyaAlby <95738218+PunyaAlby@users.noreply.github.com>
2022-01-25 15:06:46 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -35,8 +35,8 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/muhammadrizky16/Kyy-Userbot.git
-cd Geez-UserBot
+git clone https://github.com/PunyaAlby/ALBY-Userbot.git
+cd ALBY-UserBot
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
