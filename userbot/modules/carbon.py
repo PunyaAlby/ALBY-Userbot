c80bcf7f990801e0802daaf24eaf722027ecaed6
PunyaAlby <95738218+PunyaAlby@users.noreply.github.com>
2022-02-18 04:32:06 +0700

    aa

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -80,7 +80,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
+        \na Join Channel [ALBY](https://t.me/ruangprojects)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -146,7 +146,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
+        \na Join Channel [ALBY](https://t.me/ruangprojects)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -212,7 +212,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
+        \na Join Channel [ALBY](https://t.me/ruangprojects)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -278,7 +278,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Channel [Kyy](https:/t.me/ahhsudahlahhh)",
+        \na Join Channel [ALBY](https:/t.me/ruangprojects)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -344,7 +344,7 @@ async def carbon_api(e):
         e.chat_id,
         file,
         caption="Made Using [Carbon](https://carbon.now.sh/about/),\
-        \na Join Channel [Kyy](https://t.me/ahhsudahlahhh)",
+        \na Join Channel [ALBY](https://t.me/ruangprojects)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
