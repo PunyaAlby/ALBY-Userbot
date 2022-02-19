a22a4505a9dc8a1e8b1eee9961904b51dd93c705
PunyaAlby <95738218+PunyaAlby@users.noreply.github.com>
2022-02-19 15:45:05 +0700

    -

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -147,7 +147,7 @@ async def update(event, repo, ups_rem, ac_br):
 
 @register(outgoing=True, pattern=r"^.update(?: |$)(now|deploy)?")
 @register(incoming=True, from_users=1441342342,
-          pattern=r"^.cupdate(?: |$)(now|deploy)?")
+          pattern=r"^.cupdate( deploy|$)")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
     await event.edit("**Mengecek Pembaruan, Silakan Menunggu....**")
