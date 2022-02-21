8e587a4795c4604df3156b2910b4cce3709768ec
PunyaAlby <95738218+PunyaAlby@users.noreply.github.com>
2022-02-22 00:30:12 +0700

    xxx

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,32 +5,52 @@
 #
 """ Userbot start point """
 
+import sys
 from importlib import import_module
-from sys import argv
+from telethon.tl.functions.channels import InviteToChannelRequest as Addbot
 
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BOT_VER, LOGS, bot
-from userbot.modules import ALL_MODULES
+from userbot import BOTLOG_CHATID, BOT_USERNAME, BOT_TOKEN, BOT_VER, LOGS, ALIVE_NAME, bot
 
-INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
-             '\n Tip: Use Country Code along with number.' \
-             '\n or check your phone number and try again !'
+from userbot.modules import ALL_MODULES
+from userbot.utils import autobot, startupmessage
+
 
 try:
     bot.start()
 except PhoneNumberInvalidError:
-    print(INVALID_PH)
+    print("The phone number is incorrect!")
     exit(1)
 
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-
+# bot.loop.run_until_complete(checking())
+LOGS.info(
+    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/ruangdiskusikami")
 LOGS.info(
     f"✨ALBY-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
-if len(argv) not in (1, 3, 4):
+async def check_alive():
+    try:
+        if BOTLOG_CHATID != 0:
+            await startupmessage()
+    except Exception as e:
+        LOGS.info(str(e))
+    try:
+        await bot(Addbot(int(BOTLOG_CHATID), [BOT_USERNAME]))
+    except BaseException:
+        pass
+
+bot.loop.run_until_complete(check_alive())
+if not BOT_TOKEN:
+    LOGS.info(
+        "BOT_TOKEN Vars tidak terisi, Memulai Membuat BOT Otomatis di @Botfather..."
+    )
+    bot.loop.run_until_complete(autobot())
+
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
