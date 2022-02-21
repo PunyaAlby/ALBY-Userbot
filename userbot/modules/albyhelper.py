d1f6e4322ec2e40e2b400f751a295a92c143837d
PunyaAlby <95738218+PunyaAlby@users.noreply.github.com>
2022-02-22 02:23:22 +0700

    ;

--- userbot/modules/albyhelper.py
+++ userbot/modules/albyhelper.py
@@ -12,8 +12,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke @albyaja atau tag admin di @ruangdiskusikami:\n"
-        "\n[Telegram](t.me/albyaja)"
-        "\n[Repo](http://404.jodi.org/ae/)"
+        "\n[Telegram](t.me/Punya_Alby)"
+        "\n[Repo](https://github.com/PunyaAlby/ALBY-Userbot/)"
         "\n[Instagram](instagram.com/fadzkuruuniialmuttaqiin/)")
 
 
