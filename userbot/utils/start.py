70c84c321a6f95adca9b11a8b06dcea68dd70361
PunyaAlby <95738218+PunyaAlby@users.noreply.github.com>
2022-02-21 19:29:37 +0700

    b

---
+++ userbot/utils/start.py
@@ -0,0 +1,20 @@
+from telethon import Button
+
+from userbot import BOTLOG, BOTLOG_CHATID, LOGS, tgbot
+
+
+async def startupmessage():
+    """
+    Start up message in telegram logger group
+    """
+    try:
+        if BOTLOG:
+            await tgbot.send_file(
+                BOTLOG_CHATID,
+                "https://telegra.ph/file/cbe826936d4de9ec1838a.jpg",
+                caption="⚡ **ALBY-Userbot berhasil diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @SkylaIND ",
+                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/ruangdiskusikami"),)],
+            )
+    except Exception as e:
+        LOGS.error(e)
+        return None
