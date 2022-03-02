12ad26b5a94cce66cfcf55605d7acc8806996ed4
Alby <95738218+PunyaAlby@users.noreply.github.com>
2022-03-02 18:41:50 +0700

    ^_^

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -1,55 +1,83 @@
+
 # 🍀 © @tofik_dn
 # ⚠️ Do not remove credits
 
-import requests
 
+from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.events import register
+from userbot.utils import kyy_cmd
+import random
+from userbot import owner
+from telethon.tl.types import InputMessagesFilterVideo
+from telethon.tl.types import InputMessagesFilterVoice
+from telethon.tl.types import InputMessagesFilterPhotos
 
-
-@register(outgoing=True, pattern=r"^\.asupan$")
+@kyy_cmd(pattern="asupan$")
 async def _(event):
     try:
-        response = requests.get(
-            "https://api-tede.herokuapp.com/api/asupan/ptl").json()
-        await event.client.send_file(event.chat_id, response["url"])
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@AsupanCilikUserbot", filter=InputMessagesFilterVideo
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(asupannya),
+            caption=f"Nih kak asupannya [{owner}](tg://user?id={aing.id})",
+        )
         await event.delete()
     except Exception:
-        await event.edit("**Tidak bisa menemukan video asupan.**")
+        await event.edit("Tidak bisa menemukan video asupan.")
 
-
-@register(outgoing=True, pattern=r"^\.chika$")
+@kyy_cmd(pattern="desah$")
 async def _(event):
     try:
-        response = requests.get(
-            "https://api-tede.herokuapp.com/api/chika").json()
-        await event.client.send_file(event.chat_id, response["url"])
+        desahnya = [
+            desah
+            async for desah in event.client.iter_messages(
+                "@DESAHANFCE", filter=InputMessagesFilterVoice
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(desahnya),
+            caption=f"Nih kak desahannya [{owner}](tg://user?id={aing.id})",
+        )
         await event.delete()
     except Exception:
-        await event.edit("**Tidak bisa menemukan video chikakiku.**")
-
-
-@register(outgoing=True, pattern=r"^\.bocil$")
+        await event.edit("Tidak bisa menemukan desahan.")
+        
+@kyy_cmd(pattern="ayang$")
 async def _(event):
     try:
-        response = requests.get(
-            "https://api-alphabot.herokuapp.com/api/asupan/bocil?apikey=Alphabot"
-        ).json()
-        await event.client.send_file(event.chat_id, response["url"])
+        ayangnya = [
+            ayang
+            async for ayang in event.client.iter_messages(
+                "@CeweLogoPack", filter=InputMessagesFilterPhotos
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(ayangnya),
+            caption=f"Nih Ayang Aku 😘 [{owner}](tg://user?id={aing.id})",
+        )
         await event.delete()
     except Exception:
-        await event.edit("**Tidak bisa menemukan video bocil.**")
-
+        await event.edit("Gada Yang Mau Sama Kamu Karena Kamu ga Gud Luking🤪.")
 
 CMD_HELP.update(
     {
-        "asupan": "**Plugin : **`asupan`\
-        \n\n  •  **Syntax :** `.asupan`\
+        "asupan": f"**Plugin : **asupan\
+        \n\n  •  **Syntax :** {cmd}asupan\
         \n  •  **Function : **Untuk Mengirim video asupan secara random.\
-        \n\n  •  **Syntax :** `.chika`\
-        \n  •  **Function : **Untuk Mengirim video chikakiku secara random.\
-        \n\n  •  **Syntax :** `.bocil`\
-        \n  •  **Function : **Untuk Mengirim video bocil secara random.\
+        \n\n  •  **Syntax :** {cmd}ayang\
+        \n  •  **Function : **Untuk Mencari Ayang.\
+        \n\n  •  **Syntax :** {cmd}desah\
+        \n  •  **Function : **Untuk Mengirim voice desah secara random.\
     "
     }
 )
