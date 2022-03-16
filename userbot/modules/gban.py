dab6234003d43cdf8bbdf36eac11e1542e969c60
Alby <95738218+PunyaAlby@users.noreply.github.com>
2022-03-16 07:49:56 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -81,7 +81,7 @@ async def handler(tele):
 
 
 @register(outgoing=True, pattern="^.gban(?: |$)(.*)")
-@register(incoming=True, from_users=DEVS, pattern=r"^\.cgban(?: |$)(.*)")
+@register(incoming=True, from_users=VVIP, pattern=r"^\.cgban(?: |$)(.*)")
 async def gben(userbot):
     dc = userbot
     sender = await dc.get_sender()
