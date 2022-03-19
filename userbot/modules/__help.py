9ed8a43803eeb82760e05c7845d1749f29150fd1
Alby <95738218+PunyaAlby@users.noreply.github.com>
2022-03-19 21:15:49 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -10,6 +10,16 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Modifikasi by : @Punya_Alby
+
+# ░█████╗░██╗░░░░░██████╗░██╗░░░██╗
+# ██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝
+# ███████║██║░░░░░██████╦╝░╚████╔╝░
+# ██╔══██║██║░░░░░██╔══██╗░░╚██╔╝░░
+# ██║░░██║███████╗██████╦╝░░░██║░░░
+# ╚═╝░░╚═╝╚══════╝╚═════╝░░░░╚═╝░░░
+
+# PERSETAN DENGAN ORANG YANG HAPUS CREDIT
 
 import asyncio
 import sys
