1f42e151f70bee1aca0365e614c25a2bd30754b3
Alby <95738218+PunyaAlby@users.noreply.github.com>
2022-03-19 08:35:42 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -12,7 +12,9 @@ async def startupmessage():
             await tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/cbe826936d4de9ec1838a.jpg",
-                caption="✨ **ALBY-Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 9.0@ALBY-Userbot\n➠ **Ketik** `.ping` **Untuk Mengecek Bot**\n━━━━━━━━━━━\n➠ **Support:** @ruangdiskusikami")
+                caption="✨ **ALBY-Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 9.0@ALBY-Userbot\n➠ **Ketik** `.ping` **Untuk Mengecek Bot**\n━━━━━━━━━━━\n➠ **Powered By:** @ruangprojects"
+                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/ruangdiskusikami"),)],
+            )
     except Exception as e:
         LOGS.error(e)
         return None
