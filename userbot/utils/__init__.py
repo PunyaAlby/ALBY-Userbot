7804e90f8a361b9c187ab701f57d3b12c46e00a1
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-04-01 21:25:02 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -33,4 +33,11 @@ from .tools import (
     edit_or_reply,
     media_type,
 )
-from .utils import autobot, load_module, remove_plugin, start_assistant, create_supergroup
+from .utils import (
+    autobot,
+    load_module,
+    remove_plugin,
+    start_assistant,
+    create_supergroup,
+    autopilot,
+)
