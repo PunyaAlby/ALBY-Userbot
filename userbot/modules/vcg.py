079f98cbee9051ff522587b85abefd15560b917a
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-04-02 12:21:59 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

---
+++ userbot/modules/vcg.py
@@ -0,0 +1,125 @@
+# Thanks Full To Team Ultroid
+# Fiks By ALBY @punya_alby
+
+
+from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+
+from telethon.tl import types
+from telethon.utils import get_display_name
+
+from userbot import owner
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import edit_delete, edit_or_reply, alby_cmd
+from userbot.events import register
+
+NO_ADMIN = "`Maaf Kamu Bukan Admin 👮`"
+
+
+def vcmention(user):
+    full_name = get_display_name(user)
+    if not isinstance(user, types.User):
+        return full_name
+    return f"[{full_name}](tg://user?id={user.id})"
+
+
+async def get_call(alby):
+    alby = await alby.client(getchat(alby.chat_id))
+    await alby.client(getvc(alby.full_chat.call, limit=1))
+    return hehe.call
+
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i: i + n]
+
+
+@alby_cmd(pattern="startvc$")
+@register(pattern=r"^\.startvcs$", sudo=True)
+async def start_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await edit_delete(c, f"**Maaf {owner} Bukan Admin 👮**")
+        return
+    try:
+        await c.client(startvc(c.chat_id))
+        await edit_or_reply(c, "`Memulai Obrolan Suara`")
+    except Exception as ex:
+        await edit_or_reply(c, f"**ERROR:** `{ex}`")
+
+
+@alby_cmd(pattern="stopvc$")
+@register(pattern=r"^\.stopvcs$", sudo=True)
+async def stop_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await edit_delete(c, f"**Maaf {owner} Bukan Admin 👮**")
+        return
+    try:
+        await c.client(stopvc(await get_call(c)))
+        await edit_or_reply(c, "`Mematikan Obrolan Suara`")
+    except Exception as ex:
+        await edit_delete(c, f"**ERROR:** `{ex}`")
+
+
+@alby_cmd(pattern="vcinvite")
+async def _(alby):
+    await edit_or_reply(alby, "`Sedang Menginvite Member...`")
+    users = []
+    z = 0
+    async for x in alby.client.iter_participants(alby.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await alby.client(invitetovc(call=await get_call(alby), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await edit_or_reply(alby, f"`Menginvite {z} Member`")
+
+
+@alby_cmd(pattern="vctitle(?: |$)(.*)")
+@register(pattern=r"^\.cvctitle$", sudo=True)
+async def change_title(e):
+    title = e.pattern_match.group(1)
+    me = await e.client.get_me()
+    chat = await e.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not title:
+        return await edit_delete(e, "**Silahkan Masukan Title Obrolan Suara Grup**")
+
+    if not admin and not creator:
+        await edit_delete(e, f"**Maaf {me.first_name} Bukan Admin 👮**")
+        return
+    try:
+        await e.client(settitle(call=await get_call(e), title=title.strip()))
+        await edit_or_reply(e, f"**Berhasil Mengubah Judul VCG Menjadi** `{title}`")
+    except Exception as ex:
+        await edit_delete(e, f"**ERROR:** `{ex}`")
+
+
+CMD_HELP.update(
+    {
+        "vcg": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvc`\
+         \n↳ : Memulai Obrolan Suara dalam Group.\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.stopvc`\
+         \n↳ : `Menghentikan Obrolan Suara Pada Group.`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vctittle <tittle vcg>`\
+         \n↳ : `Mengubah tittle/judul Obrolan Suara.`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcinvite`\
+         \n↳ : Invite semua member yang berada di group."
+    }
+)
