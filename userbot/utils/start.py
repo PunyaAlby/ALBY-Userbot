2b8f3f9812fa759f2f1a93090b2e1c58cdf07959
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-04-05 15:18:04 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -13,7 +13,7 @@ async def startupmessage():
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/cbe826936d4de9ec1838a.jpg",
                 caption="✨ **ALBY-Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 9.0@ALBY-Userbot\n➠ **Ketik** `.ping` **Untuk Mengecek Bot**\n➠ **Ketik** `.help` **Untuk Melihat Informasi Module**\n━━━━━━━━━━━\n➠ **Powered By:** **✨ ᴀʟʙʏ ᴜꜱᴇʀʙᴏᴛ ✨**",
-                buttons=[(Button.url("ɢʀᴏᴜᴘ ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/ruangdiskusikami"),)],
+                buttons=[(Button.url("ɢʀᴏᴜᴘ ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/ruangdiskusikami"),)],\n
                 buttons=[(Button.url("ᴄʜᴀɴɴᴇʟ ᴜᴘᴅᴀᴛᴇꜱ", "https://t.me/ruangprojects"),)],
             )
     except Exception as e:
