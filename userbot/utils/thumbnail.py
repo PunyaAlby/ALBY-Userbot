d3d9f3fc738de177d37691c419eae135ae01769c
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-04-07 21:58:10 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

--- userbot/utils/thumbnail.py
+++ userbot/utils/thumbnail.py
@@ -22,27 +22,26 @@ async def gen_thumb(thumbnail, title, userid, ctitle):
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
                 f = await aiofiles.open(
-                    f"userbot/resources/thumb{userid}.png", mode="wb"
+                    f"resources/thumb{userid}.png", mode="wb"
                 )
                 await f.write(await resp.read())
                 await f.close()
     theme = random.choice(themes)
-    image1 = Image.open(f"userbot/resources/thumb{userid}.png")
-    image2 = Image.open(f"userbot/resources/{theme}.png")
+    image1 = Image.open(f"resources/thumb{userid}.png")
+    image2 = Image.open(f"resources/{theme}.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
     image6 = image4.convert("RGBA")
-    Image.alpha_composite(image5, image6).save(
-        f"userbot/resources/temp{userid}.png")
-    img = Image.open(f"userbot/resources/temp{userid}.png")
+    Image.alpha_composite(image5, image6).save(f"resources/temp{userid}.png")
+    img = Image.open(f"resources/temp{userid}.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("userbot/resources/Roboto-Light.ttf", 52)
-    font2 = ImageFont.truetype("userbot/resources/Roboto-Medium.ttf", 76)
+    font = ImageFont.truetype("resources/Roboto-Light.ttf", 52)
+    font2 = ImageFont.truetype("resources/Roboto-Medium.ttf", 76)
     draw.text((27, 538), f"Playing on {ctitle[:15]}...", (0, 0, 0), font=font)
     draw.text((27, 612), f"{title[:20]}...", (0, 0, 0), font=font2)
-    img.save(f"userbot/resources/final{userid}.png")
-    os.remove(f"userbot/resources/temp{userid}.png")
-    os.remove(f"userbot/resources/thumb{userid}.png")
-    final = f"userbot/resources/final{userid}.png"
+    img.save(f"resources/final{userid}.png")
+    os.remove(f"resources/temp{userid}.png")
+    os.remove(f"resources/thumb{userid}.png")
+    final = f"resources/final{userid}.png"
     return final
