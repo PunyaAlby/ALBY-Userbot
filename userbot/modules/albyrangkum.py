af6861a1d2bd2fc138ce6de4080e2662e0d50464
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-04-09 09:06:11 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

---
+++ userbot/modules/albyrangkum.py
@@ -0,0 +1,50 @@
+# Created by @punya_alby
+# FROM ALBY-Userbot <https://github.com/PunyaAlby/ALBY-Userbot>
+# ░█████╗░██╗░░░░░██████╗░██╗░░░██╗
+# ██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝
+# ███████║██║░░░░░██████╦╝░╚████╔╝░
+# ██╔══██║██║░░░░░██╔══██╗░░╚██╔╝░░
+# ██║░░██║███████╗██████╦╝░░░██║░░░
+# ╚═╝░░╚═╝╚══════╝╚═════╝░░░░╚═╝░░░
+# ⚠️ Do not remove credits ⚠️
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import CMD_HELP
+from userbot.events import register
+
+chat = "@awakmalas_bot"
+
+
+@register(outgoing=True, pattern="^.rangkum ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.pattern_match.group(1):
+        text, username = event.pattern_match.group(1).split()
+
+    else:
+        await event.edit("`Tambahin teks tugas kalian yang panjang agar dapat di ringkas`")
+        return
+
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_rangkum.id = await conv.send_message(f"/ringkas {text}")
+            rangkum = await conv.get_response()
+            await event.client.forward_messages(event.chat_id, rangkum)
+            await event.delete()
+        except YouBlockedUserError:
+            await event.edit(
+                "**Error: Mohon Buka Blokir** @awakmalas_bot **Dan Coba Lagi!**"
+            )
+        await event.client.delete_messages(conv.chat_id, [msg_rangkum.id, rangkum.id])
+        await event.delete()
+
+
+CMD_HELP.update(
+    {
+        "rangkum": f"❖ **Plugin : **`rangkuman teks`\
+        \n\n ┌❖ **Perintah :** `.rangkum <text>`\
+        \n └❖ **Berfungsi : **membuat rangkuman tugas kamu yang panjang menjadi singkat.\
+     "
+    }
+)
