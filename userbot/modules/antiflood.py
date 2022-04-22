6d2adeec2cfb855c14805ef6c3f75a8def11ac45
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-04-22 23:32:41 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

--- userbot/modules/antiflood.py
+++ userbot/modules/antiflood.py
@@ -20,18 +20,18 @@ async def _(event):
     # logger.info(CHAT_FLOOD)
     if not CHAT_FLOOD:
         return
-    admin_c = await is_admin(event.chat_id, event.message.from_id)
+    admin_c = await is_admin(event.chat_id, event.message.sender_id)
     if admin_c:
         return
     if not (str(event.chat_id) in CHAT_FLOOD):
         return
-    should_ban = sql.update_flood(event.chat_id, event.message.from_id)
+    should_ban = sql.update_flood(event.chat_id, event.message.sender_id)
     if not should_ban:
         return
     try:
         await event.client(EditBannedRequest(
             event.chat_id,
-            event.message.from_id,
+            event.message.sender_id,
             ANTI_FLOOD_WARN_MODE
         ))
     except Exception as e:  # pylint:disable=C0103,W0703
@@ -39,8 +39,7 @@ async def _(event):
             entity=event.chat_id,
             message="""**Automatic AntiFlooder**
 @admin [User](tg://user?id={}) is flooding this chat.
-
-`{}`""".format(event.message.from_id, str(e)),
+`{}`""".format(event.message.sender_id, str(e)),
             reply_to=event.message.id
         )
         await asyncio.sleep(10)
@@ -51,7 +50,7 @@ async def _(event):
             entity=event.chat_id,
             message="""**Automatic AntiFlooder**
 [User](tg://user?id={}) has been automatically restricted
-because he reached the defined flood limit.""".format(event.message.from_id),
+because he reached the defined flood limit.""".format(event.message.sender_id),
             reply_to=event.message.id
         )
 
