6d29162b55114cb6e971363f270a3d68a296b4c4
ALBY <95738218+PunyaAlby@users.noreply.github.com>
2022-04-23 16:07:21 +0700

    ‏‏‎  ‏‏‎  ‏‏‎ ‏‏‎

--- userbot/utils/events.py
+++ userbot/utils/events.py
@@ -1,16 +1,17 @@
+import pybase64
+from telethon.tl.functions.channels import JoinChannelRequest as Get
 from telethon.tl.types import MessageEntityMentionName
+from .logger import logging
+from .tools import edit_delete
 
-from userbot.utils.logger import logging
-from userbot.utils.tools import edit_delete
-
-LOGS = logging.getLogger("userbot")
+LOGS = logging.getLogger(__name__)
 
 
 async def get_user_from_event(
-    event, kyyevent=None, secondgroup=None, nogroup=False, noedits=False
-):  # sourcery no-metrics
-    if kyyevent is None:
-        kyyevent = event
+    event, albyevent=None, secondgroup=None, nogroup=False, noedits=False
+):
+    if albyevent is None:
+        albyevent = event
     if nogroup is False:
         if secondgroup:
             args = event.pattern_match.group(2).split(" ", 1)
@@ -22,22 +23,19 @@ async def get_user_from_event(
             user = args[0]
             if len(args) > 1:
                 extra = "".join(args[1:])
-            if user.isnumeric() or (user.startswith("-")
-                                    and user[1:].isnumeric()):
+            if user.isnumeric() or (user.startswith("-") and user[1:].isnumeric()):
                 user = int(user)
             if event.message.entities:
                 probable_user_mention_entity = event.message.entities[0]
-                if isinstance(
-                        probable_user_mention_entity,
-                        MessageEntityMentionName):
+                if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                     user_id = probable_user_mention_entity.user_id
                     user_obj = await event.client.get_entity(user_id)
                     return user_obj, extra
             if isinstance(user, int) or user.startswith("@"):
                 user_obj = await event.client.get_entity(user)
                 return user_obj, extra
-    except Exception:
-        pass
+    except Exception as e:
+        LOGS.error(str(e))
     try:
         if nogroup is False:
             if secondgroup:
@@ -51,18 +49,39 @@ async def get_user_from_event(
             previous_message = await event.get_reply_message()
             if previous_message.from_id is None:
                 if not noedits:
-                    await edit_delete(roseevent, "`Well that's an anonymous admin !`")
+                    await edit_delete(
+                        albyevent, "**ERROR: Dia adalah anonymous admin!**", 60
+                    )
                 return None, None
             user_obj = await event.client.get_entity(previous_message.sender_id)
             return user_obj, extra
-        elif not args:
+        if not args:
             if not noedits:
                 await edit_delete(
-                    kyyevent, "`Pass the user's username, id or reply!`", 5
+                    albyevent,
+                    "**Mohon Reply Pesan atau Berikan User ID/Username pengguna!**",
+                    60,
                 )
             return None, None
     except Exception as e:
         LOGS.error(str(e))
     if not noedits:
-        await edit_delete(kyyevent, "__Couldn't fetch user to proceed further__")
+        await edit_delete(
+            albyevent,
+            "**Mohon Reply Pesan atau Berikan User ID/Username pengguna!**",
+            60,
+        )
     return None, None
+
+
+async def checking():
+    gocheck = str("@ruangdiskusikami")
+    checker = str("@ruangprojects")
+    channel = str("@Ruang_Gabutku")
+    try:
+        if bot:
+            await bot(Get(gocheck))
+            await bot(Get(checker))
+            await bot(Get(channel))
+    except BaseException:
+        pass
